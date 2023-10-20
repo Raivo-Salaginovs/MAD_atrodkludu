0fa240a481ed22dd4799f511d32aa6763f8156f3
Raivo-Salaginovs <raivomadona@inbox.lv>
2023-10-20 14:36:15 +0300

    ir izpildīts 3. uzdevums

--- 3.uzdevums.py
+++ 3.uzdevums.py
@@ -1,1 +1,22 @@
-# Uzraksti programmu, kurā  dators izvēlas 100 skaitļus robe''as no 101 līdz 500. Izvēlētie skaitļi tiek izvadīti terminālī.
+# Uzraksti programmu, kurā  dators izvēlas 100 skaitļus robežās
+#  no  101 līdz 500. Izvēlētie skaitļi tiek izvadīti terminālī.
+
+import random
+
+# cik skaitļi jāizvēlas?
+skaitlis=100
+
+# kādas ir robežas?
+min_rob=100
+max_rob=501
+# jāizveido cikls, jo jāģenerē 100 skaitļi
+
+for i in range(100):
+    random_skaitlis=random.randint(min_rob, max_rob)
+    print(random_skaitlis)
+
+
+
+
+
+
